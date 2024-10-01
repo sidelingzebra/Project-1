69b43650f5afdd30c94107efd6d43bb3ea43f484
Josh. M <josh.macdonald@torontomu.ca>
2024-10-01 17:01:51 -0400

    Update Project_1.py
    Step 1 and 2 complete

--- Project_1.py
+++ Project_1.py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  1 16:17:19 2024
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sb
 
-@author: Josh
-"""
+"""Step 1: Data In"""
+df=pd.read_csv("Project_1_Data.csv")
+
+
+"""Step 2: Visalizing Data"""
+# print(df.info())
+# fig1=df.hist('X')
+# fig2=df.hist('Y')
+# fig3=df.hist('Z')
+# fig4=df.hist('Step')
+
+
+"""Step 3: Correlation Analysis"""
+sb.lmplot(data=df,x="X",y="Step")
+sb.lmplot(data=df,x="Y",y="Step")
+sb.lmplot(data=df,x="Z",y="Step")
 
