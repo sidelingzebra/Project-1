b7f5b61eaaadec6eba97281b9b2b78d415fccae7
Josh. M <josh.macdonald@torontomu.ca>
2024-10-07 14:56:53 -0400

    Update Project_1.py
    Added Correlations

--- Project_1.py
+++ Project_1.py
@@ -2,13 +2,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+
+sb.set_theme(style="darkgrid")
 
 """Step 1: Data In"""
 df=pd.read_csv("Project_1_Data.csv")
 
+#Converting Step column from Integar to 
+#df['Step']=df['Step'].astype(float)
+
 #Saving Pandas File as Array A
 A=df.to_numpy()
-print(A)
 
 
 """Step 2: Visalizing Data"""
@@ -18,7 +31,7 @@ fig2=df.hist('Y')
 fig3=df.hist('Z')
 fig4=df.hist('Step')
 
-"""3D plot creation""""
+"""3D plot creation"""
 fig5 = plt.figure()
 ax=fig5.add_subplot(projection='3d')
 
@@ -37,7 +50,11 @@ plt.show(fig5)
 
 
 """Step 3: Correlation Analysis"""
-sb.lmplot(data=df,x="X",y="Step")
-sb.lmplot(data=df,x="Y",y="Step")
-sb.lmplot(data=df,x="Z",y="Step")
+sb.lmplot(data=df,x="Step",y="X")
+sb.lmplot(data=df,x="Step",y="Y")
+sb.lmplot(data=df,x="Step",y="Z")
+
+sb.relplot(data=df,x='X',y='Y')
+
+
 
