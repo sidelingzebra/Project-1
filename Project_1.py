047174dd65d5c6546c3dce0f71778049aaa3988c
Josh. M <josh.macdonald@torontomu.ca>
2024-10-19 12:49:16 -0400

    Update Project_1.py
    Removed test code and finalized stack model

--- Project_1.py
+++ Project_1.py
@@ -28,6 +28,10 @@ from sklearn.datasets import make_classification
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.svm import SVC
 
+from matplotlib import colormaps
+from pandas.plotting import scatter_matrix
+from sklearn.ensemble import StackingClassifier
+
 
 
 sb.set_theme(style="darkgrid")
@@ -35,31 +39,25 @@ sb.set_theme(style="darkgrid")
 """Step 1: Data In"""
 df=pd.read_csv("Project_1_Data.csv")
 
-#Converting Step column from Integar to 
-#df['Step']=df['Step'].astype(float)
 
-step_cat = df[["Step"]]
-step_cat.head(4)
 
-ordinal_encoder = OrdinalEncoder()
-step_cat_encoded = ordinal_encoder.fit_transform(step_cat)
-step_cat_encoded[:13]
 
-step_cat_encoder = OneHotEncoder()
-step_1hot = step_cat_encoder.fit_transform(step_cat)
-
-step_1hot.toarray()
 
 
 
 """Step 2: Visalizing Data"""
+
+
+
+
     #Before visalizing, split data between train & test
     
     #Creates a new column that splits the data for Stratified Random Sampling
-Split_1=StratifiedShuffleSplit (n_splits=1,test_size=0.2,random_state=42)
 df["Split"] = pd.cut(df["Step"],
                           bins=[0, 2, 4, 6, np.inf],
                           labels=[1, 2, 3, 4])
+
+Split_1=StratifiedShuffleSplit (n_splits=1,test_size=0.3,random_state=24)
 
     #Removing split col that was added for SRS
 for train_index, test_index in Split_1.split(df, df["Split"]):
@@ -71,8 +69,18 @@ strat_df_test = strat_df_test.drop(columns=["Split"], axis = 1)
     #Split data into X & Y
 X_train = strat_df_train.drop("Step", axis = 1)
 y_train = strat_df_train["Step"]
+
+
 X_test = strat_df_test.drop("Step", axis = 1)
 y_test = strat_df_test["Step"]
+
+
+
+
+
+
+
+
 
     #Creating histograms of split data
 fig1, axs=plt.subplots(2,2,figsize=(10,10))
@@ -118,6 +126,12 @@ fig5.set_title('Z Regression')
 plt.show()
 
 
+attributes = ["X", "Y", "Z"]
+scatter_matrix(strat_df_train[attributes], figsize=(16, 16))
+
+plt.show()
+
+
     #Calculating Pearson Correlation for X, Y, Z with Step
 reg=r_regression(X_train,y_train)
 print("Pearson Correlation Coefficients are",reg)
@@ -131,15 +145,18 @@ scaled_data_train_df = pd.DataFrame(scaled_data_train,
                                     columns=X_train.columns[0:3])
 X_train = scaled_data_train_df.join(X_train.iloc[:,3:])
 
+print(scaled_data_train_df)
+
+
 scaled_data_test = my_scaler.transform(X_test.iloc[:,0:3])
 scaled_data_test_df = pd.DataFrame(scaled_data_test, 
-                                   columns=X_test.columns[0:3])
+                                    columns=X_test.columns[0:3])
 X_test = scaled_data_test_df.join(X_test.iloc[:,3:])
 
 
 #Print the Correlation Matrix
 plt.figure(figsize=(6, 6),dpi = 500)
-corr_matrix = (X_train).corr()
+corr_matrix = (strat_df_train).corr()
 fig6=sb.heatmap(np.abs(corr_matrix),annot=True)
 fig6.set_title('Correlation matrix')
 #sb.pairplot(strat_df_train)
@@ -162,8 +179,16 @@ print("\nBest Linear Regression Model:", best_model_lr)
 lr_prediction = grid_search_lr.predict(X_test)
 print("\nLogistic Regression Accuracy Score:", accuracy_score(lr_prediction, y_test))
 
-print(confusion_matrix(lr_prediction, y_test))
-print(classification_report(lr_prediction, y_test))
+#Logistic Regression Confusion Matrix
+fig, ax=plt.subplots(1,1,figsize=(6, 6), dpi = 500)
+ConfusionMatrixDisplay.from_predictions(lr_prediction, 
+                                            y_test,cmap=plt.cm.BuPu,ax=ax)
+ax.set_title('Logistic Regression Confusion Matrix')
+plt.show()
+
+
+# print(confusion_matrix(lr_prediction, y_test))
+print("\n",classification_report(lr_prediction, y_test))
 
 
 
@@ -173,31 +198,33 @@ print(classification_report(lr_prediction, y_test))
 
 forest_reg = RandomForestClassifier()
 
-param_grid_rf = [
-  {'n_estimators': [3, 10, 30],
-   'max_features': [2, 4, 6, 8]},
-  {'bootstrap': [False], 
-   'n_estimators': [3, 10], 
-   'max_features': [2, 3, 4]},
-  ]
-
+param_grid_rf = {
+    'n_estimators': [10, 30, 50],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['sqrt', 'log2']
+}
 grid_search_rf = GridSearchCV(forest_reg, param_grid_rf, cv=5,
-                            scoring='neg_mean_squared_error',
-                            return_train_score=True)
+                            scoring='neg_mean_absolute_error',
+                            n_jobs=-1)
 
 grid_search_rf.fit(X_train,y_train)
 
-best_model_rf = grid_search_rf.best_estimator_
-print("\nBest Random Forest Model:", best_model_rf)
-
-feature_importances_rf = grid_search_rf.best_estimator_.feature_importances_
-print("\nRandom Forest Feature Importance:", feature_importances_rf)
 
 rf_prediction = grid_search_rf.predict(X_test)
 print("\nRandom Forest Accuracy Score:", accuracy_score(rf_prediction, y_test))
 
-print(confusion_matrix(rf_prediction, y_test))
-print(classification_report(rf_prediction, y_test))
+
+#Random Forest Confusion Matrix
+fig, ax=plt.subplots(1,1,figsize=(6, 6), dpi = 500)
+ConfusionMatrixDisplay.from_predictions(rf_prediction, 
+                                            y_test,cmap=plt.cm.BuPu,ax=ax)
+ax.set_title('Random Forest Confusion Matrix')
+plt.show()
+
+# print(confusion_matrix(rf_prediction, y_test))
+print("\n",classification_report(rf_prediction, y_test))
 
 
 
@@ -207,27 +234,31 @@ print(classification_report(rf_prediction, y_test))
 decision_tree = DecisionTreeClassifier(random_state=24)
 
 param_grid_dt = {
-    'max_depth': [None, 10, 20, 30],
+    'max_depth': [10, 20, 25],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4]
 }
 grid_search_dt = RandomizedSearchCV(decision_tree, param_grid_dt, cv=5,
-                            scoring='neg_mean_squared_error',
+                            scoring='neg_mean_absolute_error',
                             n_jobs=-1)
 
 grid_search_dt.fit(X_train,y_train)
 
-best_model_dt = grid_search_dt.best_estimator_
-print("\nBest Decision Tree Model:", best_model_dt)
 
-feature_importances_dt = grid_search_dt.best_estimator_.feature_importances_
-print("\nDecision Tree Feature Importance:", feature_importances_dt)
 
 dt_prediction = grid_search_dt.predict(X_test)
 print("\nDecision Tree Accuracy Score:", accuracy_score(dt_prediction, y_test))
 
-print(confusion_matrix(dt_prediction, y_test))
-print(classification_report(dt_prediction, y_test))
+#Decision Tree Confusion Matrix
+fig, ax=plt.subplots(1,1,figsize=(6, 6), dpi = 500)
+ConfusionMatrixDisplay.from_predictions(dt_prediction, 
+                                            y_test,cmap=plt.cm.BuPu,ax=ax)
+ax.set_title('Decision Tree Confusion Matrix')
+plt.show()
+
+
+# print(confusion_matrix(dt_prediction, y_test))
+print("\n",classification_report(dt_prediction, y_test))
 
 
 
@@ -236,75 +267,63 @@ print(classification_report(dt_prediction, y_test))
 
 
 """ML Model 4 - Support Vector Machine"""
-svr = SVR()
-param_grid_svr = {
+svc = SVC()
+param_grid_svc = {
     'kernel': ['linear', 'rbf'],
     'C': [0.1, 1, 10, 100],
     'gamma': ['scale', 'auto']
 }
 
-grid_search_svr = GridSearchCV(svr, param_grid_svr, cv=5, 
+grid_search_svc = GridSearchCV(svc, param_grid_svc, cv=5, 
                                scoring='neg_mean_absolute_error', 
                                n_jobs=-1)
 
-grid_search_svr.fit(X_train, y_train)
-
-best_model_svr = grid_search_svr.best_estimator_
-print("\nBest SVM Model:", best_model_svr)
+grid_search_svc.fit(X_train, y_train)
 
 
+svc_prediction = grid_search_svc.predict(X_test)
+print("\nSupport Vector Machine Accuracy Score:", accuracy_score(svc_prediction, 
+                                                                 y_test))
 
-# Training and testing error for Linear Regression
-y_train_pred_lr = best_model_lr.predict(X_train)
-y_test_pred_lr = best_model_lr.predict(X_test)
-mae_train_lr = mean_absolute_error(y_train, y_train_pred_lr)
-mae_test_lr = mean_absolute_error(y_test, y_test_pred_lr)
-print(f"\nLinear Regression - MAE (Train): {mae_train_lr}, MAE (Test): {mae_test_lr}")
-
-# Training and testing error for Decision Tree
-y_train_pred_dt = best_model_dt.predict(X_train)
-y_test_pred_dt = best_model_dt.predict(X_test)
-mae_train_dt = mean_absolute_error(y_train, y_train_pred_dt)
-mae_test_dt = mean_absolute_error(y_test, y_test_pred_dt)
-print(f"\nDecision Tree - MAE (Train): {mae_train_dt}, MAE (Test): {mae_test_dt}")
-
-# Training and testing error for Random Forest
-y_train_pred_rf = best_model_rf.predict(X_train)
-y_test_pred_rf = best_model_rf.predict(X_test)
-mae_train_rf = mean_absolute_error(y_train, y_train_pred_rf)
-mae_test_rf = mean_absolute_error(y_test, y_test_pred_rf)
-print(f"\nRandom Forest - MAE (Train): {mae_train_rf}, MAE (Test): {mae_test_rf}")
-
-# Training and testing error for SVM
-y_train_pred_svr = best_model_svr.predict(X_train)
-y_test_pred_svr = best_model_svr.predict(X_test)
-mae_train_svr = mean_absolute_error(y_train, y_train_pred_svr)
-mae_test_svr = mean_absolute_error(y_test, y_test_pred_svr)
-print(f"\nSVM - MAE (Train): {mae_train_svr}, MAE (Test): {mae_test_svr}")
+#Support Vector Machine Confusion Matrix
+fig, ax=plt.subplots(1,1,figsize=(6, 6), dpi = 500)
+ConfusionMatrixDisplay.from_predictions(svc_prediction, 
+                                            y_test,cmap=plt.cm.BuPu,ax=ax)
+ax.set_title('Support Vector Machine Confusion Matrix')
+plt.show()
 
 
-# #Confusion Matrix through all Step Values
-# i=1
-# while i<14:
-#     y_train_i = (y_train == i) # True for all 5s, False for all other digits
-#     y_test_i = (y_test == i)
-    
-#     sgd_clf = SGDClassifier(random_state=42)
-#     sgd_clf.fit(X_train, y_train_i)
-    
-#     y_train_pred_i = cross_val_predict(sgd_clf, X_train, y_train_i, cv=3)
-#     C_mat_i=confusion_matrix(y_train_i, y_train_pred_i)
-    
-#     f1_score_i=f1_score(y_train_i, y_train_pred_i)
+print("\n",classification_report(svc_prediction, y_test))
 
-#     print("\nConfusion Matrix for: ", i, "is:\n", C_mat_i)
-#     print("\nF1 Score for: ", i, "is:\n", f1_score_i)
-#     i=i+1
 
-svc_model = SVC(random_state=24)
-svc_model.fit(X_train, y_train) 
 
-svc_prediction = svc_model.predict(X_test)
-print("\nSVC Accuracy Score:", accuracy_score(svc_prediction, y_test))
-print(confusion_matrix(svc_prediction, y_test))
-print(classification_report(svc_prediction, y_test))
+
+
+
+
+
+
+"""StackingClassifer"""
+
+stack = StackingClassifier(
+    estimators=[
+        ('lr', LogisticRegression(random_state=42)),
+        ('rf', RandomForestClassifier(random_state=42))
+    ],
+    final_estimator=RandomForestClassifier(random_state=43),
+    cv=5  # number of cross-validation folds
+)
+stack.fit(X_train, y_train)
+
+
+stack_prediction = stack.predict(X_test)
+print("\nStack Accuracy Score:", accuracy_score(stack_prediction, y_test))
+#Support Vector Machine Confusion Matrix
+fig, ax=plt.subplots(1,1,figsize=(6, 6), dpi = 500)
+ConfusionMatrixDisplay.from_predictions(stack_prediction, 
+                                            y_test,cmap=plt.cm.BuPu,ax=ax)
+ax.set_title('Stack Confusion Matrix')
+plt.show()
+
+
+print("\n",classification_report(stack_prediction, y_test))
